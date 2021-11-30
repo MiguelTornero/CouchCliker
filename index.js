a2e9d852cebb79685fcd186b6fc07978de36d3e1
Miguel Tornero <miguel.tornero.carrillo@gmail.com>
2021-11-30 17:36:13 -0600

    added signin page

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 
 const port = parseInt(process.env.PORT) || 3000;
+const clientID = process.env.GOOGLE_CLIENT_ID || "";
+const baseURL = new URL(process.env.BASE_URL).href;
 
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const { response } = require('express');
 const HandleBars = require("express-handlebars").create({
     helpers: {
         headContents(options) {
@@ -24,8 +27,11 @@ app.use(cookieParser());
 
 app.use("/", express.static("static"));
 
+const authCallback = new URL("/auth/callback", baseURL);
 app.get("/", (req, res)=>{
-    res.render("home", {name: "world"});
+    //res.render("home", {name: "world"});
+    
+    res.render("signin", {authCallback, clientID})
 });
 
 app.listen(port, ()=>{
