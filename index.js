7b45f8b8331b4edb3c80990bfb00d43af12d8826
Miguel Tornero <miguel.tornero.carrillo@gmail.com>
2021-11-30 15:43:47 -0600

    added handlebars

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const port = parseInt(process.env.PORT) || 3000;
 const app = require("express")();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const handlebars = require("express-handlebars");
+
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+
+app.get("/", (req, res)=>{
+    res.render("home", {name: "world"});
+});
 
 app.listen(port, ()=>{
     console.log(`Listening on :${port}`);
